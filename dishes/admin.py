99d18939708dae90e5ac952f67731b33cf5a6e96
Maxim Chukavov <maximchukavovmc@gmail.com>
2023-10-26 13:26:01 +0300

    add more fields in admin panel, add price_info func in DishAdmin

--- dishes/admin.py
+++ dishes/admin.py
@@ -5,10 +5,26 @@ from dishes.models import Basket, Dish, DishCategory
 
 @admin.register(Dish)
 class DishAdmin(admin.ModelAdmin):
-    list_display = ['id', 'name', 'price', 'category']
+    list_display = ['id', 'name', 'price', 'weight', 'description', 'category', 'price_info']
+    list_editable = ['name', 'price', 'weight', 'category']
     search_fields = ['name']
-    ordering = ['name']
+    ordering = ['id']
+    list_per_page = 10
     prepopulated_fields = {'slug': ('name',)}
+    actions = ['default_price']
+
+    @admin.display(description='Стоимость')
+    def price_info(self, dish: Dish):
+        if dish.price < 500:
+            return "Низкая цена"
+        elif 500 < dish.price < 1000:
+            return "Средняя цена"
+        else:
+            return 'Цена выше среднего'
+
+    @admin.action(description="Установить цену 0")
+    def default_price(self, request, queryset):
+        queryset.update(price=0)
 
 
 @admin.register(DishCategory)
