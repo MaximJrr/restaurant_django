a7769d837a1517cdd9f806504068df64103c6e03
Maxim Chukavov <maximchukavovmc@gmail.com>
2023-10-29 20:41:25 +0300

    add fields in admin panel, add two func, add field in class User

--- users/admin.py
+++ users/admin.py
@@ -6,11 +6,26 @@ from users.models import EmailVerification, User, Reservation
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ['id', 'username',  'image', 'email', 'is_verified_email']
-    list_editable = ['is_verified_email']
-    list_display_links = ['username']
-    ordering = ['id']
+    list_display = ['id', 'username', 'first_name', 'last_name', 'email', 'is_verified_email',
+                    'check_is_verified_email', 'image'
+    ]
+    list_editable = ['username', 'first_name', 'last_name', 'is_verified_email']
+    search_fields = ['username', 'first_name', 'last_ name', 'email']
+    ordering = ['username']
+    actions = ['delete_image']
+    list_per_page = 10
     inlines = (BasketAdmin,)
+
+    @admin.action(description="Удалить изображение у выбранных пользователей")
+    def delete_image(self, request, queryset):
+        queryset.update(image=None)
+
+    @admin.display(description="Статус подтверждения почты")
+    def check_is_verified_email(self, user: User):
+        if user.is_verified_email:
+            return 'Почта подтверждена'
+        elif user.is_verified_email is False:
+            return 'Почта не подтверждена'
 
 
 @admin.register(EmailVerification)
