3f6fb1d7afdce219315c79a11ecd174a7102d5bc
Maxim Chukavov <maximchukavovmc@gmail.com>
2023-10-29 20:09:32 +0300

    add field in admin panel, add class Meta in model, fixed templates

--- orders/admin.py
+++ orders/admin.py
@@ -5,5 +5,10 @@ from orders.models import Order
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ['__str__', 'status']
+    list_display = ['id', 'first_name', 'last_name', 'email', 'address', 'created', 'status']
+    search_fields = ['id', 'first_name', 'last_name', 'email']
+    list_display_links = ['first_name', 'last_name']
+    ordering = ['-created']
+    list_editable = ['address', 'status']
+    list_per_page = 10
     readonly_fields = ['id', 'created']
