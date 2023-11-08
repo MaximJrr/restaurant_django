876cf41e8335a0d37d16a60416553828b64f4bd0
Maxim Chukavov <maximchukavovmc@gmail.com>
2023-11-08 18:57:35 +0300

    fixed urls

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from users.views import (EmailVerificationView, UserLoginView,
-                         UserRegisterView, Basket, logout, ReservationView, ReservationVerification)
+                         UserRegisterView, BasketView, logout, ReservationView, ReservationVerificationView)
 
 app_name = 'users'
 
@@ -9,8 +9,8 @@ urlpatterns = [
     path('login', UserLoginView.as_view(), name='login'),
     path('register/', UserRegisterView.as_view(), name='register'),
     path('logout/', logout, name='logout'),
-    path('basket/', Basket.as_view(), name='basket'),
+    path('basket/', BasketView.as_view(), name='basket'),
     path('email_verification/<str:email>/<uuid:unique_code>/', EmailVerificationView.as_view(), name='email_verification'),
     path('reservation/', ReservationView.as_view(), name='reservation'),
-    path('reservation_verification', ReservationVerification.as_view(), name='reservation_verification')
+    path('reservation_verification', ReservationVerificationView.as_view(), name='reservation_verification')
 ]
