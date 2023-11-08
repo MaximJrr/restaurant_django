d2e2d307f045631ebd48d1b19a97c03f192091aa
Maxim Chukavov <maximchukavovmc@gmail.com>
2023-11-08 15:51:01 +0300

    add extra_context, add BasketView

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from users.views import (EmailVerificationView, UserLoginView,
-                         UserRegisterView, basket, logout, ReservationView, ReservationVerification)
+                         UserRegisterView, Basket, logout, ReservationView, ReservationVerification)
 
 app_name = 'users'
 
@@ -9,7 +9,7 @@ urlpatterns = [
     path('login', UserLoginView.as_view(), name='login'),
     path('register/', UserRegisterView.as_view(), name='register'),
     path('logout/', logout, name='logout'),
-    path('basket/', basket, name='basket'),
+    path('basket/', Basket.as_view(), name='basket'),
     path('email_verification/<str:email>/<uuid:unique_code>/', EmailVerificationView.as_view(), name='email_verification'),
     path('reservation/', ReservationView.as_view(), name='reservation'),
     path('reservation_verification', ReservationVerification.as_view(), name='reservation_verification')
