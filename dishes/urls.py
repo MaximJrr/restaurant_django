768f562aaeb64ed41f5842308a9f507c6564bcc7
Maxim Chukavov <maximchukavovmc@gmail.com>
2023-11-09 21:48:22 +0300

    rename class

--- dishes/urls.py
+++ dishes/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 
-from dishes.views import DishesListView, CategoryListView, basket_add, basket_remove
+from dishes.views import MenuView, CategoryListView, basket_add, basket_remove
 
 app_name = 'dishes'
 
 urlpatterns = [
-    path('', DishesListView.as_view(), name='menu'),
+    path('', MenuView.as_view(), name='menu'),
     path('category/<slug:category_slug>/', CategoryListView.as_view(), name='category'),
-    path('page/<int:page>/', DishesListView.as_view(), name='paginator'),
+    path('page/<int:page>/', MenuView.as_view(), name='paginator'),
     path('add/<int:dish_id>/', basket_add, name='basket_add'),
     path('baskets/remove/<int:basket_id>/', basket_remove, name='basket_remove'),
 ]
